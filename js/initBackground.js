29df6b182567f3a7849c770297b1e5450ccdbecd
Deunoh <denovann@live.fr>
2024-01-18 21:52:59 +0100

    bug init resolu

--- js/initBackground.js
+++ js/initBackground.js
@@ -6,8 +6,12 @@ const initBackground = {
 
         bodyBackground.style.backgroundImage = `url(${fullImageFromStorage})`;
         madeby.innerHTML = `<a id="artist" href="https://instagram.com/${instaImageFromStorage}">Made by ${ArtistImageFromStorage}</a>`;
-
-    }
+        console.log('img par défaut', fullImageFromStorage);
+        if(fullImageFromStorage == null && ArtistImageFromStorage == null){
+            bodyBackground.style.backgroundImage = 'url(logo/wallpaper1.jpg)';
+            madeby.innerHTML = '<a id="artist" href="#">Made by Denovann</a>';
+        }
+    },
 
 }
 
